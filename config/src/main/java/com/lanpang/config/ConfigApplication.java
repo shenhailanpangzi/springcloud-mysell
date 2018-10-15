8cad14d031cd7fd4ed9ab45019e461ed81eab482
lanpangzi <18835708323@163.com>
2018-10-15 15:06:49 +0800

    对称加密 github账号

--- config/src/main/java/com/lanpang/config/ConfigApplication.java
+++ config/src/main/java/com/lanpang/config/ConfigApplication.java
@@ -1,16 +1,28 @@
 package com.lanpang.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
+@RestController
 public class ConfigApplication {
+    @Value("${spring.cloud.config.server.git.username}")
+    private String username;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
+
+    @GetMapping("/print")
+    public String print() {
+        return "username:" + username;
+    }
+
 }
